ca551b58e7584c25ccd0606dfd43dd08261b8edd
aj9045 <identity_unveiled@hotmail.com>
2014-12-20 16:05:15 -0500

    add content for post #create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -46,6 +46,30 @@ describe UsersController do
   end
 
   describe "Post #create" do
+    context "valid attributes" do
+      it "saves user into database" do
+        expect { post :create, user: attributes_for(:user) }.to change(User, :count).by(1)
+      end
+
+      it "redirects to root" do
+        post :create, user: attributes_for(:user)
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "sets session for that user" do
+        post :create, user: attributes_for(:user)
+        expect(session[:user_id]).to_not be_nil
+      end
+    end
+    context "invalid attributes" do
+      it "should not save into the database" do
+        expect { post :create, user: attributes_for(:invalid_user) }.to_not change(User, :count)
+      end
+      it "should re-render :new template" do
+        post :create, user: attributes_for(:invalid_user)
+        expect(response).to render_template(:new)
+      end
+    end
   end
 
   describe "Put #update" do
