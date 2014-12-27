fabdb492906f45212eb5e76180d23fc08e5b2e4e
aj9045 <identity_unveiled@hotmail.com>
2014-12-27 12:33:25 -0500

    add content to migration users

--- db/migrate/20141227171921_create_users.rb
+++ db/migrate/20141227171921_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username, null: false
+      t.string :name, null: false
+      t.string :password_digest, null: false
+      t.string :about
+
+      t.timestamps
     end
   end
 end
