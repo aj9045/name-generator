deb2d424831f0f488aeb938295c9f316cc9d52f2
aj9045 <identity_unveiled@hotmail.com>
2014-12-27 14:35:22 -0500

    delete email bit

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :user do
     username "user"
     sequence(:name) { |n| "name#{n}" }
-    sequence(:email) { |n| "test#{n}@dbc.com" }
     password "pswrd"
     password_confirmation "pswrd"
 
