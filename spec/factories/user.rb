cd37bf9421a5ef41c4c518820a3b3d84466766f1
aj9045 <identity_unveiled@hotmail.com>
2014-12-28 13:13:14 -0500

    change user attributes

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    username "user"
+    sequence(:username) { |n| "usernumber#{n}"}
     sequence(:name) { |n| "name#{n}" }
     password "pswrd"
     password_confirmation "pswrd"
