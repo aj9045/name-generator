e198e6debd73416f19417b43f79f44bc3afc5437
aj9045 <identity_unveiled@hotmail.com>
2015-01-01 22:50:30 -0500

    randomly generate names in the welcome controller in order ot have them show in the generator

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    generator = Generator.new
+    @random_last_name = generator.last_name
+    @random_first_name = generator.first_name
   end
 end
